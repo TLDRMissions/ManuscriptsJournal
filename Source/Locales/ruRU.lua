7ed9dd5a78e55235b64ff64322bf8eb81b87d93b
teelolws <49792789+teelolws@users.noreply.github.com>
2023-04-17 23:16:28 +1200

    Update ruRU.lua

--- Source/Locales/ruRU.lua
+++ Source/Locales/ruRU.lua
@@ -1,5 +1,5 @@
 local addonName, addon = ...
-local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "ruRU", true)
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "ruRU")
 if not L then return end
 
 -- Russian translation by ZamestoTV
