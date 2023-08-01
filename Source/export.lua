54f97df143ec75f2d5839bef808fc3ad091c384e
Teelo <49792789+teelolws@users.noreply.github.com>
2023-08-01 12:19:20 +1200

    Add export to saved variables module

---
+++ Source/export.lua
@@ -0,0 +1,17 @@
+local addonName, addon = ...
+
+-- This is a LoadOnDemand addon so we shouldn't need to do any ADDON_LOADED event checks
+-- But just in case someone changes the TOC or manages to make this load during startup, I'll add a delayed rescan anyway.
+
+local function exportDB()
+    ManuscriptsJournalExportDB = {}
+    ManuscriptsJournalExportDB.version = C_AddOns.GetAddOnMetadata(addonName, "Version")
+    ManuscriptsJournalExportDB.learnedManuscripts = {}
+
+    for _, manuscriptData in pairs(addon.db) do
+        ManuscriptsJournalExportDB.learnedManuscripts[manuscriptData.questID] = C_QuestLog.IsQuestFlaggedCompleted(manuscriptData.questID)
+    end
+end
+
+exportDB()
+C_Timer.After(20, exportDB)
